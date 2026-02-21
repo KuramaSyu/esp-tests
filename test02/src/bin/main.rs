5a49c2d3639512f2a109f083e1e74b590b70b819
kurama <52566613+KuramaSyu@users.noreply.github.com>
2026-02-21 15:52:25 +0100

    feat: read temp from ds18b20
    using 3v, gnd and GPIO3 for data

--- test02/src/bin/main.rs
+++ test02/src/bin/main.rs
@@ -7,11 +7,14 @@
 )]
 #![deny(clippy::large_stack_frames)]
 
-use defmt::info;
+use defmt::{error, info};
 use esp_hal::clock::CpuClock;
+use esp_hal::delay::Delay;
+use esp_hal::gpio::{DriveMode, Flex, OutputConfig, Pull};
 use esp_hal::main;
 use esp_hal::time::{Duration, Instant};
 use esp_hal::timer::timg::TimerGroup;
+use onewire::{DS18B20, DeviceSearch, OneWire, Sensor, ds18b20};
 use panic_rtt_target as _;
 
 extern crate alloc;
@@ -43,9 +46,58 @@ fn main() -> ! {
     let (mut _wifi_controller, _interfaces) =
         esp_radio::wifi::new(&radio_init, peripherals.WIFI, Default::default())
             .expect("Failed to initialize Wi-Fi controller");
+    let mut delay = esp_hal::delay::Delay::new();
+
+    // let mut one = peripherals.GPIO3.i
+    let mut onewire_pin = Flex::new(peripherals.GPIO3);
+
+    // open drain + pull up config
+    let open_drain_config = OutputConfig::default()
+        .with_drive_mode(DriveMode::OpenDrain)
+        .with_pull(Pull::Up);
+
+    onewire_pin.apply_output_config(&open_drain_config); // set pin to open drain config
+    onewire_pin.set_output_enable(true); // enable the config
+    onewire_pin.set_input_enable(true); // enable reading
+    onewire_pin.set_level(esp_hal::gpio::Level::High); // start at idle which is when pin is HIGH
+
+    // one wire instance
+    let mut onewire = OneWire::new(&mut onewire_pin, false);
+
+    // setup http client
 
     loop {
-        info!("Hello world!");
+        if onewire.reset(&mut delay).is_err() {
+            // missing pullup or error on line
+            loop {}
+        }
+
+        // search for devices
+        let mut search = DeviceSearch::new();
+        while let Some(device) = onewire.search_next(&mut search, &mut delay).unwrap() {
+            match device.address[0] {
+                ds18b20::FAMILY_CODE => {
+                    let ds18b20 = DS18B20::new(device).unwrap();
+
+                    // request sensor to measure temperature
+                    let resolution = ds18b20
+                        .measure_temperature(&mut onewire, &mut delay)
+                        .unwrap();
+
+                    // wait for compeltion, depends on resolution
+                    delay.delay_micros(resolution.time_ms() as u32);
+
+                    // read temperature
+                    match ds18b20.read_measurement(&mut onewire, &mut delay) {
+                        Ok(temperature) => info!("Temperature: {}C", temperature),
+                        Err(_e) => error!("Failed to read Temerature"),
+                    }
+                }
+                _ => {
+                    // unknown device type
+                }
+            }
+        }
         let delay_start = Instant::now();
         while delay_start.elapsed() < Duration::from_millis(500) {}
     }
